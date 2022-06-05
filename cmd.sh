c97e376229e7594946011dba5a029efe982fc21f
qiaofeng1227 <76487013@qq.com>
2022-06-05 16:28:48 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # customrized cmd powered by Websoft9
 
-sed -i "s/dbName :*\dbName : '$MYSQL_DB'/g" /www/zentaopms/module/install/control.php
-sed -i "s/dbUser :*\dbName : '$MYSQL_USER'/g" /www/zentaopms/module/install/control.php
-sed -i "s/dbPassword :*\dbName : '$MYSQL_PASSWORD'/g" /www/zentaopms/module/install/control.php
+sed -i "s/dbName :*/dbName : '$MYSQL_DB'/g" /www/zentaopms/module/install/control.php
+sed -i "s/dbUser :*/dbName : '$MYSQL_USER'/g" /www/zentaopms/module/install/control.php
+sed -i "s/dbPassword :*/dbName : '$MYSQL_PASSWORD'/g" /www/zentaopms/module/install/control.php
 
 bash /.docker_init.sh
 tail -f /dev/null
