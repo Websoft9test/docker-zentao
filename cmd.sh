6e8a7cae49e0bd36f95099027906c69da83a1665
qiaofeng1227 <76487013@qq.com>
2022-06-05 16:24:24 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -5,5 +5,5 @@ sed -i "s/dbName :*\dbName : '$MYSQL_DB'/g" /www/zentaopms/module/install/contro
 sed -i "s/dbUser :*\dbName : '$MYSQL_USER'/g" /www/zentaopms/module/install/control.php
 sed -i "s/dbPassword :*\dbName : '$MYSQL_PASSWORD'/g" /www/zentaopms/module/install/control.php
 
-/.docker_init.sh
+bash /.docker_init.sh
 tail -f /dev/null
