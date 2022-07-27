8e1a5f20ed86a2872a02955fd18c863a23dcb64b
qiaofeng1227 <76487013@qq.com>
2022-07-27 13:00:21 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -4,4 +4,4 @@
 sed -i "s/dbName : 'zentao'/dbName : '$MYSQL_DB'/g" /apps/zentao/module/install/control.php
 sed -i "s/dbPassword : ''/dbPassword : '$MYSQL_PASSWORD'/g" /apps/zentao/module/install/control.php
 
-bash /.docker_init.sh
+bash /apps/zentao/bin/init.sh
